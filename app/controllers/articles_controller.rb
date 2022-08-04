6ee601aafaf55a28020e12e119e1c4041e644b36
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-04 16:32:28 -0500

    commit tree 3999a38aa9ca7a8031f730adb86cc32d0da26ac6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,28 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  #
   def index
+    
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    # session[:page_views] ||= 0
+    if session[:page_views]
+      session[:page_views] += 1
+    else
+      session[:page_views] = 1
+    end
+
     article = Article.find(params[:id])
-    render json: article
+    # render json: {session: session}
+    if session[:page_views] <= 3
+      render json: article
+    else
+      render json: {error: "Maximum pageview limit reached"}, status: 401
+    end
   end
 
   private
